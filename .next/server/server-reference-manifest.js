5f8eff18619b0fefa19e4cadd7b9963f94f5a37a
edu gonzalez <320ego@gmail.com>
2025-05-24 18:26:05 +0000

    f

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7+IRJOHyq53MS8t58Q4x9bw5siVmi5oPC/1hEyM5hbk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/chunks/src_app_presse_page_tsx_9148afbf._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_823307df._.js"
-],
-    source: "dynamic"
-});
